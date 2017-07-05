954f98dde73492629133f72b8c1414e3f8b91825
embs <embs@cin.ufpe.br>
2017-07-04 23:39:04 -0300

    temp: Trying out etcd Java clients
    Note: interaction with etcd is not a responsibility for jsc.

--- msm-jsc/src/main/java/msm/sc/Application.java
+++ msm-jsc/src/main/java/msm/sc/Application.java
@@ -3,10 +3,23 @@ package msm.sc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.coreos.jetcd.Client;
+import com.coreos.jetcd.ClientBuilder;
+import com.coreos.jetcd.KV;
+import com.coreos.jetcd.api.RangeResponse;
+
+import java.util.concurrent.CompletableFuture;
+
 @SpringBootApplication
 public class Application {
 
   public static void main(String[] args) {
+    Client client = ClientBuilder.newBuilder().endpoints("http://localhost:2379").build();
+    KV kvClient = client.getKVClient();
+    CompletableFuture<RangeResponse> getFuture = kvClient.get(key);
+    RangeResponse response = getFuture.get();
+    System.out.print("Response: ");
+    System.out.println(response);
     SpringApplication.run(Application.class, args);
   }
 }
