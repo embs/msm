e80adce776b0ab514c50aecf59c29d7480c96b1c
embs <embs@cin.ufpe.br>
2017-07-05 18:54:32 -0300

    msm-jsc: Enhance information sent on registration

--- msm-jsc/src/main/java/msm/sc/MainController.java
+++ msm-jsc/src/main/java/msm/sc/MainController.java
@@ -10,7 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
 
   @RequestMapping("/register")
-  public void register(@RequestParam(value="name") String name) {
-    ProducerSing.i().send(new ProducerRecord<String,String>("register", name));
+  public void register(@RequestParam(value="name") String name,
+                       @RequestParam(value="host") String host,
+                       @RequestParam(value="port") String port) {
+    ProducerRecord<String, String> record;
+    final String TOPIC = "register";
+    String address = String.format("%s:%s", host, port);
+    record = new ProducerRecord<String, String>(TOPIC, name, address);
+
+    ProducerSing.i().send(record);
   }
 }
